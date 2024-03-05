add48bd06329c54a625ba76a0471b2484548662b
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-03-05 10:28:29 +0200

    Initial commit from Create Next App

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <div>
+            <h3 className="text-3xl font-bold text-center">CHIRUZA BISIMWA GRACE</h3>
+        </div>
+    )
+}
+
+export default HomePage
