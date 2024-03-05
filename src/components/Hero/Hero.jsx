ba599acf10af14ba69f2e16e613b59ba3387b4e6
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-03-05 16:51:53 +0200

    add: success

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                     <div>
                         <h1 className="text-5xl font-bold">Racine14 Tech Solution</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-success">Get Started</button>
                     </div>
                 </div>
             </div>
