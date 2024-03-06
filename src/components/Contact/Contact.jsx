8222eb32d3b0840e4b91b8eef39d0941c6bcc2ff
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-03-06 14:12:21 +0200

    add: style css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
                             <div className="group mt-5">
                                 <button
                                     type="submit"
-                                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="rounded-md bg-success px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-success"
                                 >
                                     Send message
                                 </button>
